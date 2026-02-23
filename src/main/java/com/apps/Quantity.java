62b07567395e6255e2e18783a2d9afdeecd1ad93
harshita_dubey25 <harshi.d2507@gmail.com>
2026-02-23 16:59:31 +0530

    [Harshita] : add Quantity class

--- src/main/java/com/apps/Quantity.java
+++ src/main/java/com/apps/Quantity.java
@@ -3,6 +3,7 @@ package com.apps;
 public class Quantity {
 	private double value;
 	private Unit unit;
+	private static final double EPSILON =  1e-3;
 	
 	public Quantity(double value, Unit unit) {
 		if (unit == null) {
@@ -22,22 +23,39 @@ public class Quantity {
 	
 	
 	@Override
-	public boolean equals(Object obj) {
-
-		if (this == obj)
+	public boolean equals(Object o) {
+		if (this == o) {
 			return true;
+		}
 
-		if (obj == null || getClass() != obj.getClass())
+		if (o == null || getClass() != o.getClass()) {
 			return false;
+		}
 
-		Quantity that = (Quantity) obj;
-
-		return Double.compare(this.convertToBaseUnit(), that.convertToBaseUnit()) == 0;
+		Quantity that = (Quantity) o;
+		double thisValue = this.convertToBaseUnit();
+		double thatValue = that.convertToBaseUnit();
+		return Math.abs(thisValue - thatValue) < EPSILON;
+	}
+	
+	@Override
+	public int hashCode() {
+	    long normalized =
+	        Math.round(convertToBaseUnit() / EPSILON);
+	    return Long.hashCode(normalized);
 	}
 	
 	public static void main(String[] args) {
 		Quantity length1 = new Quantity(1.0, Unit.FEET);
 		Quantity length2 = new Quantity(12.0, Unit.INCHES);
 		System.out.println("Are lengths equal? " + length1.equals(length2)); // true
+		
+		Quantity length3 = new Quantity(1.0, Unit.YARD);
+		Quantity length4 = new Quantity(36.0, Unit.INCHES);
+		System.out.println("Are lengths equal? " + length3.equals(length4)); // true
+		
+		Quantity length5 = new Quantity(100.0, Unit.CENTIMETERS);
+		Quantity length6 = new Quantity(39.3701, Unit.INCHES);
+		System.out.println("Are lengths equal? " + length5.equals(length6)); // true
 	}
 }
