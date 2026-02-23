4c3b7229a8c5d39e0d7954cf7a1c6f383303316e
harshita_dubey25 <harshi.d2507@gmail.com>
2026-02-23 15:56:55 +0530

    [Harshita] : add GenericLength feature

--- src/main/java/com/apps/QuantityMeasurementApp.java
+++ src/main/java/com/apps/QuantityMeasurementApp.java
@@ -4,83 +4,36 @@ import java.util.*;
 
 public class QuantityMeasurementApp {
 	
-	// Inner class for feet measurement
-	public static class Feet{
-		public final double value;
-		
-		public Feet(double value) {
-			this.value = value;
+	// Static method to demonstrate Length equality
+		public static boolean demonstrateLengthEquality(Quantity l1, Quantity l2) {
+			return l1.equals(l2);
 		}
-		
-		public double getValue() {
-			return value;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if(this ==  obj) {
-				return true;
-			}
-			
-			if(obj == null || getClass() != obj.getClass()) {
-				return false;
-			}
-			
-			Feet other = (Feet) obj;
-			return (Double.compare(this.value,other.value) == 0);
-		}
-		
-		@Override
-		public int hashCode() {
-			return Double.hashCode(value);
-		}
-	}
-	
-	// Inner class for Inch measurement
-	public static class Inches{
-		public final double value;
-		
-		public Inches(double value) {
-			this.value = value;
-		}
-		
-		public double getValue() {
-			return value;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if(this ==  obj) {
-				return true;
-			}
-			
-			if(obj == null || getClass() != obj.getClass()) {
-				return false;
-			}
-			
-			Inches other = (Inches) obj;
-			return (Double.compare(this.value,other.value) == 0);
-		}
-		
-		@Override
-		public int hashCode() {
-			return Double.hashCode(value);
-		}
-	}
-	
-	public static void demonstrateFeetEquality() {
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
-		System.out.println("Feet Equal: " + f1.equals(f2));
-	}
 
-	public static void demonstrateInchesEquality() {
-		Inches i1 = new Inches(1.0);
-		Inches i2 = new Inches(1.0);
-		System.out.println("Inches Equal: " + i1.equals(i2));
-	}
-	public static void main(String[] args) {
-		demonstrateFeetEquality();
-		demonstrateInchesEquality();
-	}
+		// Static method to demonstrate Feet equality
+		public static void demonstrateFeetEquality() {
+			Quantity feet1 = new Quantity(1.0, Unit.FEET);
+			Quantity feet2 = new Quantity(1.0, Unit.FEET);
+			System.out.println("Feet equality: " + demonstrateLengthEquality(feet1, feet2));
+		}
+
+		// Static method to demonstrate Inches equality
+		public static void demonstrateInchesEquality() {
+			Quantity inch1 = new Quantity(1.0, Unit.INCHES);
+			Quantity inch2 = new Quantity(1.0, Unit.INCHES);
+			System.out.println("Inches equality: " + demonstrateLengthEquality(inch1, inch2));
+		}
+
+		// Static method to demonstrate Feet and Inches comparison
+		public static void demonstrateFeetInchesComparison() {
+			Quantity feet1 = new Quantity(1.0, Unit.FEET);
+			Quantity inch12 = new Quantity(12.0, Unit.INCHES);
+			System.out.println("Feet vs Inches equality: " + demonstrateLengthEquality(feet1, inch12));
+		}
+
+		// Main method
+		public static void main(String[] args) {
+			demonstrateFeetEquality();
+			demonstrateInchesEquality();
+			demonstrateFeetInchesComparison();
+		}
 }
